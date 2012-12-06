4d4826fff1e2fd41a10308f6ec400946fefc3b0b
Mik Freedman and Nick Greenfield <pair+mik+greenie@pivotallabs.com>
2012-12-06 15:25:52 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omniauthwrapper::Application.config.secret_token = '87655b9f34aac623fff7013d2cc563c6a1e285b4fdc8e1ad9a583e6449f92ce8d0df1514dcedcb43dda5454a5e60c781a7533e56d482da552aca58306ce4f58c'
